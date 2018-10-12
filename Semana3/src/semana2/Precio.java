b3946b7e20ce743dc600e28c6bde8b31d6294e0e
Carlos Juca <chjuca@utpl.edu.ec>
2018-10-12 10:24:10 -0500

    Agregando Ejercicios Semana2

---
+++ Semana3/src/semana2/Precio.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana2;
+
+/**
+ *
+ * @author Carlos Juca
+ */
+public class Precio {
+
+    double euros;
+
+    public double obtener() {
+        double x;
+        return x;
+    }
+
+    public void agregar(double x) {
+
+    }
+}
